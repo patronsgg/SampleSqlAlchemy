e35f7d79e6e8c4eae8ec0aa4b0c63294d2f8d2cd
Max <patronsgg@yandex.ru>
2021-04-08 00:36:49 +0300

    Complete tasks "Форма регистрации"

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
 from data.jobs import Jobs
+from data.users import User
+from forms.user import RegisterForm
 
 app = Flask(__name__)
+
+app.secret_key = 'assdasdasdasdasd'
 
 
 @app.route('/')
@@ -10,6 +14,35 @@ def news_work():
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs).all()
     return render_template('index.html', jobs=jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            email=form.email.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
